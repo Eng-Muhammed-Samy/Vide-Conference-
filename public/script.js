df8b0a69f5020dfdb229a2e04da64f48c7a9d6de
Eng-Muhammed-Samy <eng_muhamed_samy@yahoo.com>
2020-12-23 00:51:30 +0200

    add mute and unmute button , add stop and start video

--- public/script.js
+++ public/script.js
@@ -14,7 +14,7 @@ const peers = {}
 let myVideoStream;
 navigator.mediaDevices.getUserMedia({
     video: true,
-    audio: false
+    audio: true
 }).then(stream => {
     myVideoStream = stream;
     addVideoSream(myVideo, stream)
@@ -71,5 +71,61 @@ $('html').keydown((e) => {
 })
 
 socket.on('createMessage', message => {
-    $('ul').append(`<li class = "message"><b>user </b>${message}</li>`)
-})
+        $('ul').append(`<li class = "message"><b>user </b>${message}</li>`)
+    })
+    // mute our video
+const muteUnmute = () => {
+    const enabled = myVideoStream.getAudioTracks()[0].enabled;
+    if (enabled) {
+        myVideoStream.getAudioTracks()[0].enabled = false;
+        setUnmuteButton();
+    } else {
+        setMuteButton();
+        myVideoStream.getAudioTracks()[0].enabled = true;
+    }
+};
+
+const setMuteButton = () => {
+    const html = `
+    <i class="fas fa-microphone"></i>
+    <span>Mute</span>
+  `;
+    document.querySelector(".main__mute_button").innerHTML = html;
+};
+
+const setUnmuteButton = () => {
+    const html = `
+    <i class="unmute fas fa-microphone-slash"></i>
+    <span>Unmute</span>
+  `;
+    document.querySelector(".main__mute_button").innerHTML = html;
+};
+
+
+
+const playStop = () => {
+    let enabled = myVideoStream.getVideoTracks()[0].enabled;
+    if (enabled) {
+        myVideoStream.getVideoTracks()[0].enabled = false;
+        setPlayVideo();
+    } else {
+        setStopVideo();
+        myVideoStream.getVideoTracks()[0].enabled = true;
+    }
+};
+
+const setStopVideo = () => {
+    const html = `
+    <i class="fas fa-video"></i>
+    <span>Stop Video</span>
+  `;
+    document.querySelector(".main__video_button").innerHTML = html;
+};
+
+const setPlayVideo = () => {
+    const html = `
+  <i class="stop fas fa-video-slash"></i>
+    <span>Play Video</span>
+  `;
+    document.querySelector(".main__video_button").innerHTML = html;
+};
